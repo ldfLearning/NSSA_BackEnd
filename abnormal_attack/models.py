62201dc914d3ff68a9ce162791b06ed77373e96b
dachilles <dachilles0112@gmail.com>
2023-11-22 20:44:43 +0800

    add: abnormal host model

--- abnormal_attack/models.py
+++ abnormal_attack/models.py
@@ -10,3 +10,11 @@ class Traffic(models.Model):
     src_ip = models.CharField(max_length=20)    # 源IP
     dst_ip = models.CharField(max_length=20)    # 目的IP
     detail = models.CharField()                 # 详细信息
+
+# 异常主机发现表
+class Host(models.Model):
+    id = models.AutoField(primary_key=True)             # 自增主键
+    ip = models.CharField(max_length=20)                # 资产IP
+    name = models.CharField(max_length=100, default='') # 资产名称
+    errprint = models.CharField(max_length=200, default='')  # 异常信息
+    time = models.CharField(max_length=50, default='')  # 异常时间
--- abnormal_host/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AbnormalHostConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'abnormal_host'

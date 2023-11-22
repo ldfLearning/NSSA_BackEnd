4d785d8e0e87d51b58b82187222eea1472ddb1b1
su_mu_zhe <2331810232@qq.com>
2023-11-22 20:52:41 +0800

    add_Admin

--- abnormal_attack/models.py
+++ abnormal_attack/models.py
@@ -18,3 +18,12 @@ class Host(models.Model):
     name = models.CharField(max_length=100, default='') # 资产名称
     errprint = models.CharField(max_length=200, default='')  # 异常信息
     time = models.CharField(max_length=50, default='')  # 异常时间
+
+# 用户行为检测表
+class Admin(models.Model):
+    id = models.AutoField(primary_key=True)     # 自增主键
+    type = models.IntegerField(default=0)       # 攻击类型
+    time = models.DateTimeField()               # 发现时间
+    user_name = models.CharField(max_length=50) # 用户姓名
+    topic = models.CharField(max_length=200)    # 主题名称
+    src_ip = models.CharField(max_length=20)    # IP

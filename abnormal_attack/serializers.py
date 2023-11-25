6e4eab28e27fa5a098938131c37e1bfb4631ecca
wangxin327 <2331810232@qq.com>
2023-11-25 15:00:33 +0000

    add_user

--- abnormal_attack/serializers.py
+++ abnormal_attack/serializers.py
@@ -5,3 +5,8 @@ class AbnormalTrafficSerializer(serializers.ModelSerializer):
     class Meta:
         model = AbnormalTraffic
         fields = '__all__'
+
+class AbnormalUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AbnormalUser
+        fields = '__all__'
--- abnormal_attack/urls.py
+++ abnormal_attack/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from api.traffic import AbnormalTrafficListAPIView, AbnormalTrafficDetailAPIView
+from api.user import AbnormalUserListAPIView, AbnormalUserDetailAPIView
 
 urlpatterns = [
     path('traffics', AbnormalTrafficListAPIView.as_view()),
     path('traffic', AbnormalTrafficDetailAPIView.as_view()),
+    path('users', AbnormalUserListAPIView.as_view()),
+    path('user', AbnormalUserDetailAPIView.as_view()),
 ]

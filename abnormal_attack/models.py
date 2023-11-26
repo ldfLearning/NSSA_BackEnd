b58241776b31ab1b389c4d3789a43d708ecbeb9f
szh0105 <693076883@qq.com>
2023-11-26 22:34:22 +0800

    feat:models.py

--- abnormal_attack/models.py
+++ abnormal_attack/models.py
@@ -28,11 +28,11 @@ class AbnormalTraffic(models.Model):
 
 # 异常主机发现表
 class AbnormalHost(models.Model):
-    id = models.AutoField(primary_key=True)  # 自增主键
-    ip = models.CharField(max_length=20)  # 资产IP
-    name = models.CharField(max_length=100, default='')  # 资产名称
-    errprint = models.CharField(max_length=200, default='')  # 异常信息
-    time = models.DateTimeField()  # 发现时间
+    id = models.AutoField(primary_key=True)                 # 自增主键
+    ip = models.CharField(max_length=20)                    # 资产IP
+    name = models.CharField(max_length=100, default='')     # 资产名称
+    detail = models.TextField()                             # 异常信息
+    time = models.DateTimeField()                           # 发现时间
 
 
 # 用户行为检测表

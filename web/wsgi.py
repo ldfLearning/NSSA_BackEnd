aee187386e4c43c29fa34c150f8f3c0932a922c3
ldfLearning <>
2023-12-03 16:33:40 +0800

    add:添加邮件发送功能

--- web/wsgi.py
+++ web/wsgi.py
@@ -25,10 +25,37 @@ import json
 from kafka import KafkaConsumer
 from abnormal_attack.models import AbnormalTraffic, AbnormalHost, AbnormalUser
 from incident_response.models import IncidentEvent
+from incident_response.models import EmailSettings
+from django.conf import settings
+from django.core.mail import send_mail
+from email.utils import formataddr
 
 MSG_TYPE_TRAFFIC = 0
 MSG_TYPE_HOST = 1
 MSG_TYPE_USER = 2
+
+# 查询最后一条的邮件设置记录
+def getEmailSettings():
+    try:
+        latest_email_settings = EmailSettings.objects.latest('id')
+        return latest_email_settings
+    except Exception as e:
+        print(e)
+
+#发送邮件
+def send_email_view(warning_msg):
+    try:
+        emailsetting = getEmailSettings()
+        subject = emailsetting.email_subject #主题
+        from_name = emailsetting.email_addresser_name  # 发件人显示
+        from_email = settings.EMAIL_HOST_USER  # 发件人邮箱
+        message = warning_msg
+        recipient_list = [emailsetting.email_recipient]  # 收件人邮箱列表
+        # 使用formataddr函数设置发件人名称和邮箱地址
+        from_address = formataddr((from_name, from_email))
+        send_mail(subject, message, from_address, recipient_list)
+    except Exception as e:
+        print(e)
 
 
 def dealMessageEvents(topic, group_id, bootstrap_servers):
@@ -51,21 +78,27 @@ def dealMessageEvents(topic, group_id, bootstrap_servers):
             traffic.save()
             # 记录日志
             traffic_log = IncidentEvent(
-                type='AbnormalAttackTraffic', time=data.time, finished=False, content=json.dumps(data)
+                type='AbnormalAttackTraffic', ip=data.dst_ip, time=data.time, finished=False, detail=json.dumps(data)
             )
             traffic_log.save()
+            # 发送邮件
+            warning_msg = data.time+',  '+data.dst_ip+'的机器正在遭遇异常流量威胁，详细情况如下：'+data.detail
+            send_email_view(warning_msg)
 
         # 异常主机
         elif msg.type == MSG_TYPE_HOST:
             # 保存异常主机信息
             traffic = AbnormalHost(
-                ip=data.ip, name=data.name, errprint=data.errprint, time=data.time)
+                ip=data.ip, name=data.name, detail=data.detail, time=data.time)
             traffic.save()
             # 记录日志
             host_log = IncidentEvent(
-                type='AbnormalAttackHost', time=data.time, finished=False, content=json.dumps(data)
+                type='AbnormalAttackHost', ip=data.ip, time=data.time, finished=False, detail=json.dumps(data)
             )
             host_log.save()
+            # 发送邮件
+            warning_msg = data.time+',  '+data.ip+'的机器存在异常主机行为，详细情况如下：'+data.detail
+            send_email_view(warning_msg)
 
         # 异常用户
         elif msg.type == MSG_TYPE_USER:
@@ -75,9 +108,12 @@ def dealMessageEvents(topic, group_id, bootstrap_servers):
             traffic.save()
             # 记录日志
             user_log = IncidentEvent(
-                type='AbnormalAttackUser', time=data.time, finished=False, content=json.dumps(data)
+                type='AbnormalAttackUser', ip=data.src_ip, time=data.time, finished=False, detail=json.dumps(data)
             )
             user_log.save()
+             # 发送邮件
+            warning_msg = data.time+',  '+data.src_ip+'的机器存在异常用户行为，详细情况如下：'+data.topic
+            send_email_view(warning_msg)
 
 # 多线程任务
 import threading

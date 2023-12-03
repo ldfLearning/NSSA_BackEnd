2012104d67a5800923a115e915f39ba716cf9266
ldfLearning <>
2023-12-03 16:37:14 +0800

    add:添加相关功能;feat:修改相关设置

--- incident_response/serializers.py
+++ incident_response/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from incident_response.models import IncidentEvent
+from incident_response.models import IncidentEvent,EmailSettings
 
 class IncidentEventSerializer(serializers.ModelSerializer): 
     class Meta:
         model = IncidentEvent
+        fields = '__all__'
+
+class EmailSettingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmailSettings
         fields = '__all__'

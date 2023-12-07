89a1b5a63cfab519ff64250f7144b72e25b5340b
ldfLearning <>
2023-12-07 15:26:09 +0800

    feat:修改流量监测逻辑，在api中维护流量值

--- flow_monitoring/api/flow.py
+++ flow_monitoring/api/flow.py
@@ -1,37 +1,63 @@
 from response import CustomResponse, ERROR_CODES
 from http import HTTPStatus
-from datetime import timedelta
+from datetime import datetime, timedelta
 from django.utils import timezone
 from rest_framework.views import APIView
-from flow_monitoring.models import NetworkTraffic
+from django.conf import settings
+import psutil
+from collections import deque
+
+# 初始化历史的网络流量信息，使用队列存储最新的15条，在api维护流量值
+history_network_traffic = deque(maxlen=15)
 
 class FlowAPIView(APIView):
+    def get_range_record(self,history_network_traffic,start_timestamp,end_timestamp):
+        selected_records = [record for record in history_network_traffic if start_timestamp <= record[0] <= end_timestamp]          
+        return selected_records
+    
     def get(self, request):
         try:
-            query_count = int(request.GET.get('count', 1))
+            #防止前端传过来的时间戳包含小数
+            start_timestamp = int(float(request.GET.get('start_timestamp'))) #开始时间戳，考虑代码执行时间，时间戳精确到秒级int
+            end_timestamp = int(float(request.GET.get('end_timestamp')))   #结束时间戳
+            step = int(request.GET.get('step'))
 
-            # 从数据库中获取最新的数据
-            latest_data = NetworkTraffic.objects.order_by('-timestamp')[:query_count + 1]
+             # 获取当前的网络流量信息
+            current_network_traffic = psutil.net_io_counters(pernic=True)
+            # 从配置文件中读取默认网卡名称
+            default_network_interface = settings.DEFAULT_NETWORK_INTERFACE
+            # 计算当前流量包数
+            packets = current_network_traffic[default_network_interface].packets_sent + current_network_traffic[default_network_interface].packets_recv
+            # 记录当前时间戳
+            record_time = int(datetime.now().timestamp())
 
-            database_count = len(latest_data)
+            # print(record_time)
 
-            total_traffic_data = []
-            # 计算流量差值
-            for i in range(0,database_count-1, 1):  
-                traffic_data = latest_data[i].total_packets - latest_data[i + 1].total_packets
-                total_traffic_data.append({'time': latest_data[i].timestamp, 'traffic': traffic_data})
+            # 将当前包数量和记录时间存储
+            history_network_traffic.append((record_time, packets))
 
-            history_time = latest_data[database_count-1].timestamp if database_count-1 > 0 else timezone.now()
+            # print(history_network_traffic)
 
-            # 填充total_traffic_data到query_count条
-            while len(total_traffic_data) < query_count:
-                  total_traffic_data.append({'time': history_time, 'traffic': 0})
-                  history_time = history_time - timedelta(minutes=2)
+            # 计算在时间范围内的包数量
+            records = self.get_range_record(history_network_traffic,start_timestamp,end_timestamp)
+            packets_in_ranges = []
+            step_timestamp = start_timestamp + step
 
-            response_data = {
-                'total_traffic': total_traffic_data,
-            }
-            return CustomResponse(data=response_data)
+            while step_timestamp <= end_timestamp :
+                  tmp_records = self.get_range_record(records,start_timestamp,step_timestamp)
+                #   print(tmp_records)
+                  if tmp_records:
+                     packets_in_range = tmp_records[-1][1] - tmp_records[0][1]
+                  else:
+                     packets_in_range = 0 
+                  packets_in_ranges.append({'timestamp':step_timestamp,'packets':packets_in_range})
+                  start_timestamp = step_timestamp
+                  step_timestamp = start_timestamp + step
+          
+            return CustomResponse(data={
+                'count':len(packets_in_ranges),
+                'total_packets':packets_in_ranges
+            })
         except Exception as e:
             return CustomResponse(
                 code=ERROR_CODES['INTERNAL_SERVER_ERROR'],
--- flow_monitoring/models.py
+++ flow_monitoring/models.py
@@ -1,6 +1,1 @@
 from django.db import models
-
-class NetworkTraffic(models.Model):
-    timestamp = models.DateTimeField(auto_now_add=True)
-    interface_name = models.CharField(max_length=50)
-    total_packets = models.BigIntegerField()

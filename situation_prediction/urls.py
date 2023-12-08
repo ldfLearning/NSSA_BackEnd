16b65f4ca0ada7dee15ecead85e992db24969ea6
Yan Kun <yk6367@qq.com>
2023-12-08 19:14:43 +0800

    [fix]: move view to api
    Signed-off-by: Yan Kun <yk6367@qq.com>

--- situation_prediction/urls.py
+++ situation_prediction/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from situation_prediction.views import SituationPredictionView, SituationPredictionHistoryView
+from .api.situation_prediction import SituationPredictionView, SituationPredictionHistoryView
 
 urlpatterns = [
     path('value/', SituationPredictionView.as_view()),

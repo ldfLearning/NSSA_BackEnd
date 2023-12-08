ce04c73f2d492db86279a1e12e6a5ad69d87d84e
Yan Kun <yk6367@qq.com>
2023-12-08 19:17:03 +0800

    [fix]: rename filename
    Signed-off-by: Yan Kun <yk6367@qq.com>

--- situation_prediction/urls.py
+++ situation_prediction/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .api.situation_prediction import SituationPredictionView, SituationPredictionHistoryView
+from .api.situation import SituationPredictionView, SituationPredictionHistoryView
 
 urlpatterns = [
     path('value/', SituationPredictionView.as_view()),
